db02d71c45c302da99c49c9bb4150160f8064745
lunajos <lunajose003@gmail.com>
2019-11-12 13:50:47 -0500

    Create Automation.py

--- Finish/Automation.py
+++ Finish/Automation.py
@@ -1,6 +1,27 @@
+import os
+import shutil
+count = 0
 
-# Python's Standard Library
-# pip installed modules
+for root, dirs, files in os.walk("./Data/ovpn_src/", topdown=True):
 
-import sys
-import importlib
+    for name in files:
+
+        namelist = name.split(".", 5)
+        country = namelist[0][0:2]
+        protocol = namelist[3][0:3]
+
+        newname = namelist[0][3:] + "_config_" + namelist[3][3:]  +  ".ovpn"
+
+        destination = os.path.join(os.getcwd(), "Data", "ovpn_dest"  )
+        source = root + name
+
+        if not os.path.isdir(os.path.join(destination,country)):
+            os.mkdir(os.path.join(destination, country))
+
+        if not os.path.isdir(os.path.join(destination, country, protocol)):
+            os.mkdir(os.path.join(destination, country, protocol))
+
+        count +=1
+        shutil.copyfile(source, os.path.join( destination, country, protocol, name))
+
+print("Successfully Oganized " + str(count) + " Files")
