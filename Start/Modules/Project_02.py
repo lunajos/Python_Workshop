13b69d1471cd2c900991960b54a0711ee2d86d3e
lunajos <lunajose003@gmail.com>
2019-11-13 11:33:13 -0500

    Add beginning files and comments

---
+++ Start/Modules/Project_02.py
@@ -0,0 +1,10 @@
+# Create a Restaurant class
+
+    # Define __init__ with params
+
+    # Create a print menu method
+
+    # Return available tables
+
+
+    # Create Dine in method
---
+++ Start/Project_03.py
@@ -0,0 +1,18 @@
+# Import Project_02.py from Module folder
+
+# Create dictionary of Items on menu
+
+# Create instance of the Restaurtnt Class
+# Number of tables,
+# Name of Restaurant
+# Dictionary of menu items
+
+# Call the print method
+
+# Call Dine In method
+
+# Show number of available tables
+
+# Create a new instance
+
+# Repeat ...
