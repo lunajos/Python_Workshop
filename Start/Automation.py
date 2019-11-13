1c49084d927e170cf0037ffae39921459eb7c449
lunajos <lunajose003@gmail.com>
2019-11-13 10:38:31 -0500

    Update Start of project

--- Start/Automation.py
+++ Start/Automation.py
@@ -1,6 +1,19 @@
+# Import Modules
+# like OS
+# and shutil
 
-# Python's Standard Library
-# pip installed modules
+# Look through each source folder
+# Read each line, Decide how to split
+# Decide how to group files
 
-import sys
-import importlib
+# Extract country
+
+# Extract protocol
+
+# Create Country Directory if not exists
+
+# Create Protocol Directory if not exists
+
+# Get New location
+
+# Move or Copy file into new location
