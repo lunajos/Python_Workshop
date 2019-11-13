167e88617af450dbd28515c2c23705a1d047f59b
lunajos <lunajose003@gmail.com>
2019-11-13 10:37:32 -0500

    Organize into module

---
+++ Start/Modules/module.py
@@ -0,0 +1,31 @@
+# Create a restaurant class
+# Define __init__ with params
+# Assign Values
+
+# Create a create order method
+# Create a pick up method
+# Create a dine in method
+# Show available tables
+# Show menu
+
+class restaurant:
+    def __init__(object, tables, restaurantName):
+        object.num = num
+        object.tables = tables
+        object.restaurantName = restaurantName
+
+    def showMenu(self):
+        print("****************************************")
+        print("           " + object.restaurantName + "")
+        print("\n\n\n\n\n")
+        print("Soup:\t $2.95")
+        print("Salad:\t $5.55")
+        print("Pasta:\t $7.35")
+        print("Hoagie:\t $5.95")
+        print("****************************************")
+
+    def createOrder():
+      pass
+
+    def dineIn():
+      pass
